bc3e88ad2b9e55c9680c7faa8ee642fa5b6ef981
Sander Meis <sander.meis@gmail.com>
2020-12-21 00:29:26 +0100

    finished 7.1

---
+++ ex7.1/main.cpp
@@ -0,0 +1,32 @@
+//
+//
+// b) Increases the value of the key by 1 everytime the key (word) appears. So counts words
+//
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <string>
+
+int main() {
+    std::ifstream file("example.txt");
+    if (!file.fail()) {
+        std::cout << "Stream opened successfully" << std::endl;
+    }
+    std::map<std::string, int> myMap;
+
+    std::string word;
+    while (file >> word) {
+        myMap[word] += 1;
+    }
+
+std::map<std::string, int>::iterator it;
+
+for (auto i = myMap.begin(); i!=myMap.end(); i++){
+    std::cout << i->second << "x " << i->first << std::endl;
+}
+
+    return 0;
+}
